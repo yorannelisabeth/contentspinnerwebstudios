8ed71033f5b07e4e951d141985c9c7d5f43c7ba2
yorann <yorannelisabeth@gmail.com>
2019-09-27 11:27:39 +0200

    update sauvegarde2 file

--- contentspinner+crud(tania)/sauvegarde2.php
+++ contentspinner+crud(tania)/sauvegarde2.php
@@ -1,4 +1,4 @@
-<?php
+        <?php
 require "common.php";
 
 function spin($txt)
@@ -31,68 +31,52 @@ function getDatabaseConnexion()
 
 function process($param_txt)
 {
-      ini_set('memory_limit','2048M');
-      ini_set('max_execution_time', 300);
-      set_time_limit(300);
+    ini_set('memory_limit', '2048M');
+    ini_set('max_execution_time', 300);
+    set_time_limit(300);
     $region = file_get_contents('population-francaise-par-departement-2018.json');
     $docregion = json_decode($region, true);
     $cities = file_get_contents('cities.json');
     $doccities = json_decode($cities, true);
 
-
-
-
-
     foreach ($docregion as $getdata) {
         $departement = $getdata['field']['departement'];
-        $codeDepartement=$getdata['field']['code_departement'];
+        $codeDepartement = $getdata['field']['code_departement'];
 
-        foreach($doccities as $data)
+        foreach ($doccities as $data) {
+            $zipcode = $data['zip_code'];
+            $ville = $data['ville_nom_simple'];
+            $citydepartment = ['ville_departement'];
+            $size = count($ville);
+            if ($codeDepartement === $citydepartment) {
 
-        
-        
-        $zipcode = $data['zip_code'];
-        $ville=$data['ville_nom_simple'];
-        $citydepartment=['ville_departement']
-        $size = count($ville);
-        
+                for ($i = 0; $i <= $size - 1; $i++) {
 
-        for ($i = 0; $i <= $size - 1; $i++) {
-            if($codeDepartement === $citydepartment){
-                    echo $departement;
-                try {
-                    $variable2 = $_POST['texte'];
-                    $spinvariable2 = spin($variable2);
-    
-                    $search2 = array(":ville", ":codepostale",":departement");
-                    $replacers2 = array($ville, $zipcode, $departement);
-    
-                    $txt2 = str_replace($search2, $replacers2, $spinvariable2);
-                    echo $txt2, '<br />';
-    
-                    $con = getDatabaseConnexion();
-                    $sql = "INSERT INTO users (texte, titre) VALUES ('$txt2')";
-                    $con->exec($sql);
-                } catch (PDOException $e) {
-                    echo $sql . "<br>" . $e->getMessage();
-    
+                    try {
+                        $variable2 = $_POST['texte'];
+                        $spinvariable2 = spin($variable2);
+
+                        $search2 = array(":ville", ":codepostale", ":departement");
+                        $replacers2 = array($ville, $zipcode, $departement);
+
+                        $txt2 = str_replace($search2, $replacers2, $spinvariable2);
+                        echo $txt2, '<br />';
+
+                        $con = getDatabaseConnexion();
+                        $sql = "INSERT INTO users (texte, titre) VALUES ('$txt2')";
+                        $con->exec($sql);
+                    } catch (PDOException $e) {
+                        echo $sql . "<br>" . $e->getMessage();
+
+                    }
+
                 }
 
-
-
-
-
             }
-
-            
-
         }
 
     }
 
-
-
-    
 }
 
 // process();
@@ -101,4 +85,4 @@ if (isset($_POST["submit"])) {
     $texte = $_POST["texte"];
     process($texte);
 }?>
-<a href="index.php">retour à l'acceuil </a>
+    <a href="index.php">retour à l'acceuil </a>
